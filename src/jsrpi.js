7cb75ba573a11f7f1968eeda0c986065ac42655f
Chris Brunner <chris.brunner@enosix.com>
2017-02-02 19:50:43 -0500

    Bump framework version.

--- src/jsrpi.js
+++ src/jsrpi.js
@@ -1,7 +1,7 @@
 "use strict";
 //
 //  jsrpi.js
-//  Version 0.1.2 ALPHA
+//  Version 0.2.2 ALPHA
 //
 //  Author:
 //       Chris Brunner <cyrusbuilt at gmail dot com>
@@ -28,7 +28,7 @@
  * @module jsrpi
  */
 
-var JSRPI_FRAMEWORK_VER = '0.1.2';
+const JSRPI_FRAMEWORK_VER = '0.2.2';
 
 module.exports = {
   /**
